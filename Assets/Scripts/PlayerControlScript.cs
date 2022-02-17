7818156b92e38273ea195d3494c95e5f52eb39d8
Hunter O'Connor <hcoconnor2001@yahoo.com>
2022-02-17 16:14:20 -0500

    added walking
    -added walking
    -modified camera controls to work with new player object structure

--- Assets/Scripts/PlayerControlScript.cs
+++ Assets/Scripts/PlayerControlScript.cs
@@ -8,10 +8,18 @@ public class PlayerControlScript : MonoBehaviour
     public Transform playerCamTrans;
 
     public float camSensitivity;
+    public float playerWalkSpeed;
+    public float jumpForce;
+
+    public SphereCollider groundCheck;
 
 
     Rigidbody playerRB;
     Transform playerTrans;
+
+
+    float moveVert = 0;
+    float moveHorz = 0;
 
 
 
@@ -21,6 +29,7 @@ public class PlayerControlScript : MonoBehaviour
     void Start()
     {
         playerTrans = transform;
+        playerRB = GetComponent<Rigidbody>();
     }
 
     void Update()
@@ -37,24 +46,79 @@ public class PlayerControlScript : MonoBehaviour
         {
             //walking cam controls
 
-            playerTrans.Rotate(0, mouseX, 0);
+            playerCamTrans.Rotate(0, mouseX, 0);
             playerCamTrans.Rotate(mouseY, 0, 0);
+
             //clamp vertical camera angle
             playerCamTrans.localEulerAngles = new Vector3(
-                Mathf.Clamp(playerCamTrans.localEulerAngles.x, 5, 60), 
-                0,
+                Mathf.Clamp(playerCamTrans.localEulerAngles.x, 5, 60),
+                playerCamTrans.localEulerAngles.y,
                 0
                 );
 
 
+            //walking moving controls
+            moveVert = Input.GetAxis("Vertical");
+            moveHorz = Input.GetAxis("Horizontal");
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                //do jump if on ground; else switch to flying
+                if (Physics.CheckSphere(groundCheck.transform.position,
+                        groundCheck.radius,
+                        LayerMask.NameToLayer("Ground")))
+                {
+                    //is on ground, so jump
+
+                    playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                    Debug.Log("jump");
+                }
+                else
+                {
+                    // is not on ground, so switch to flying
+                    Debug.Log("not jump");
+                }
+
+            }
         }
 
     }
 
     void FixedUpdate()
     {
-        
 
+        //apply movement forces
+
+        if (flying)
+        {
+            //flying movement forces
+
+        }
+        else
+        {
+            //walking movement forces
+
+
+            //Matrix4x4 camRotMat = Matrix4x4.Rotate(Quaternion.Euler(0,playerCamTrans.rotation.y,0));
+            //Vector3 moveVector = camRotMat.MultiplyPoint3x4(new Vector3(moveVert, 0, moveHorz));
+            //Debug.Log(camRotMat);
+            Transform camDir = new GameObject().transform;
+            camDir.rotation = Quaternion.Euler(0, playerCamTrans.rotation.eulerAngles.y, 0);
+
+            Vector3 moveVector = Vector3.RotateTowards(new Vector3(0, 0, moveVert),
+                                                    Mathf.Sign(moveVert)*camDir.forward,
+                                                    Mathf.PI,0);
+            moveVector += Vector3.RotateTowards(new Vector3(moveHorz, 0,0),
+                                                    Mathf.Sign(moveHorz) * camDir.right,
+                                                    Mathf.PI, 0);
+            
+            //Debug.Log(moveVector);
+            playerRB.MovePosition(playerTrans.position+moveVector.normalized * Time.fixedDeltaTime*playerWalkSpeed);
+            Destroy(camDir.gameObject);
+
+
+            
+        }
 
 
 
