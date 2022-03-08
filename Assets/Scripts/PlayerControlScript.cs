ac89ee87297bc1eb4eee48851d8a91a547129c21
Hunter O'Connor <hcoconnor2001@yahoo.com>
2022-03-08 18:33:34 -0500

    fixed walk speed curve
    fixed walk speed curve to actually work
    added 180 reverse instead of turn

--- Assets/Scripts/PlayerControlScript.cs
+++ Assets/Scripts/PlayerControlScript.cs
@@ -8,7 +8,10 @@ public class PlayerControlScript : MonoBehaviour
     public Transform playerCamTrans;
 
     public float camSensitivity;
-    public float playerWalkSpeed;
+    public float maxWalkSpeed;
+    //public float walkSpeedScale;
+    public float walkAccelerationTime = 1; //in seconds
+    public AnimationCurve walkCurve;
     public float jumpForce;
 
     public SphereCollider groundCheck;
@@ -21,7 +24,9 @@ public class PlayerControlScript : MonoBehaviour
     float moveVert = 0;
     float moveHorz = 0;
 
-
+    Vector3 intendedMoveVec;
+    float intendedMoveTime;
+    Vector3 actualMoveVec;
 
     bool flying = false;
 
@@ -30,6 +35,8 @@ public class PlayerControlScript : MonoBehaviour
     {
         playerTrans = transform;
         playerRB = GetComponent<Rigidbody>();
+        actualMoveVec = new Vector3(0, 0, 0);
+
     }
 
     void Update()
@@ -60,6 +67,82 @@ public class PlayerControlScript : MonoBehaviour
             //walking moving controls
             moveVert = Input.GetAxis("Vertical");
             moveHorz = Input.GetAxis("Horizontal");
+
+            Transform camDir = new GameObject().transform;
+            camDir.rotation = Quaternion.Euler(0, playerCamTrans.rotation.eulerAngles.y, 0);
+
+            Vector3 lastIntendedVec = intendedMoveVec;
+            float lastIntendedMoveTime = intendedMoveTime;
+
+            intendedMoveVec = Vector3.RotateTowards(new Vector3(0, 0, moveVert),
+                                                    Mathf.Sign(moveVert) * camDir.forward,
+                                                    Mathf.PI, 0);
+            intendedMoveVec += Vector3.RotateTowards(new Vector3(moveHorz, 0, 0),
+                                                    Mathf.Sign(moveHorz) * camDir.right,
+                                                    Mathf.PI, 0);
+            intendedMoveVec.Normalize();
+            if(intendedMoveVec.magnitude == 0)
+            {
+                //no movement input; slow character
+                intendedMoveTime = Mathf.Clamp(lastIntendedMoveTime - Time.deltaTime,0,walkAccelerationTime);
+                
+            }
+            else
+            {
+                
+
+                //there are movement inputs, speed up character in right direction.
+                intendedMoveTime = Vector3.Project(lastIntendedVec, intendedMoveVec).magnitude;
+                intendedMoveTime += Time.deltaTime;
+                
+
+                if(actualMoveVec.magnitude == 0)
+                {
+                    actualMoveVec = intendedMoveVec;
+                }
+                else
+                {
+                    //Vector3 lastMoveVec = actualMoveVec;
+                   // Debug.Log(intendedMoveVec);
+                    if(180 - Vector3.Angle(actualMoveVec,intendedMoveVec) <= 2.5)
+                    {
+                        //reverse speed
+                        intendedMoveTime = Mathf.Clamp(intendedMoveTime - 2.5f*Time.deltaTime, 0, walkAccelerationTime);
+                        Debug.Log(intendedMoveTime);
+                    }
+                    else
+                    {
+                        //turn toward new direction
+                        actualMoveVec = Vector3.RotateTowards(actualMoveVec, intendedMoveVec, Mathf.PI / 4 * Time.fixedDeltaTime, 0);
+                    }
+                    
+                }
+                intendedMoveVec *= Mathf.Clamp(intendedMoveTime, 0, walkAccelerationTime);
+
+            }
+            
+
+            Destroy(camDir.gameObject);
+
+
+            //float speed = Vector3.Project(actualMoveVec, lastMoveVec).magnitude;
+
+            //calculate new speed to match acceleration curve of (percent of max speed) = x^2.5 (for 0<=x<=1)
+
+            //float newSpeed = Mathf.Clamp(speed * walkSpeedScale,0,maxWalkSpeed);
+
+
+            float newSpeed = maxWalkSpeed*walkCurve.Evaluate(intendedMoveTime/walkAccelerationTime);
+                
+                
+                
+            //Debug.Log(newSpeed);
+            actualMoveVec.Normalize();
+            actualMoveVec *= newSpeed;
+
+
+            
+
 
             if (Input.GetButtonDown("Jump"))
             {
@@ -105,25 +188,46 @@ public class PlayerControlScript : MonoBehaviour
             //Matrix4x4 camRotMat = Matrix4x4.Rotate(Quaternion.Euler(0,playerCamTrans.rotation.y,0));
             //Vector3 moveVector = camRotMat.MultiplyPoint3x4(new Vector3(moveVert, 0, moveHorz));
             //Debug.Log(camRotMat);
-            Transform camDir = new GameObject().transform;
-            camDir.rotation = Quaternion.Euler(0, playerCamTrans.rotation.eulerAngles.y, 0);
 
-            Vector3 moveVector = Vector3.RotateTowards(new Vector3(0, 0, moveVert),
-                                                    Mathf.Sign(moveVert)*camDir.forward,
-                                                    Mathf.PI,0);
-            moveVector += Vector3.RotateTowards(new Vector3(moveHorz, 0,0),
-                                                    Mathf.Sign(moveHorz) * camDir.right,
-                                                    Mathf.PI, 0);
-            
             //Debug.Log(moveVector);
-            playerRB.MovePosition(playerTrans.position+moveVector.normalized * Time.fixedDeltaTime*playerWalkSpeed);
-            Destroy(camDir.gameObject);
+
+            //if(actualMoveVec.magnitude == 0)
+            //{
+            //    playerRB.AddForce(playerRB.velocity * -5);
+            //}
+            //else
+            //{
+               //Debug.Log(actualMoveVec);
+            //    playerRB.AddForce(actualMoveVec, ForceMode.Acceleration);
+            //}
+
+            Vector3 newVelocity = actualMoveVec;
+            newVelocity.y += playerRB.velocity.y;
+
+            playerRB.velocity = newVelocity;
 
 
-            
+
         }
 
 
 
     }
+
+    //float calcNewSpeed(float currentSpeed, float maxSpeed, float accel)
+    //{
+    //    //calculate new speed based on speed curve of (percent of max Speed) = x^(exp)
+    //    //return (x+accel)^exp
+
+    //    //float x = Mathf.Pow(currentSpeed/maxSpeed, 1 / exp);
+    //    //float newX = Mathf.Clamp(x+accel,0,1);
+    //    //float percentOfMaxSpeed = Mathf.Pow(newX,exp);
+    //    //return percentOfMaxSpeed * maxSpeed;
+
+        
+
+    //    return maxSpeed *+ walkCurve.
+
+
+    //}
 }
